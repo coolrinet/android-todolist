5ad7f4a8f5269337d4701b39574278410226c1a5
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-01-09 15:25:34 +0700

    Create todo database class

---
+++ app/src/main/java/com/coolrinet/todolist/data/TodoDatabase.kt
@@ -0,0 +1,12 @@
+package com.coolrinet.todolist.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    version = 1,
+    entities = [Todo::class]
+)
+abstract class TodoDatabase : RoomDatabase() {
+    abstract val todoDao: TodoDao
+}
