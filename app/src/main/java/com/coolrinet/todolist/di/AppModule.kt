a11426446d274688c5bc5a5c0ae622dceed4214e
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2024-01-09 15:33:38 +0700

    Set up dependency injection with Hilt

---
+++ app/src/main/java/com/coolrinet/todolist/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.coolrinet.todolist.di
+
+import android.app.Application
+import androidx.room.Room
+import com.coolrinet.todolist.data.TodoDatabase
+import com.coolrinet.todolist.data.TodoRepository
+import com.coolrinet.todolist.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private const val DATABASE_NAME = "todo_list"
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(application: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            application,
+            TodoDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(database: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(database.todoDao)
+    }
+}
